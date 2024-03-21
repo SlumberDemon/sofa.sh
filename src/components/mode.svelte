628540827ff84ccdddfd9ae9e2ea140e17b58d52
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2024-03-21 14:15:54 +0100

    fix a11y warnings

--- src/components/mode.svelte
+++ src/components/mode.svelte
@@ -18,6 +18,8 @@
     <div
       class="bg-rose-600 rounded-full w-12 h-12 hover:scale-110 transition ease-in-out duration-500 cursor-pointer"
       on:click={modeSwitcher}
+      role="button"
+      tabindex="0"
     />
   </div>
 </div>
