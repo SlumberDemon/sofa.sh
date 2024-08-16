e4f9350d67ddafc31a8a4199acef2797ba429b50
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2024-08-16 13:18:57 +0200

    try auto adapter

--- .vercel/output/static/_app/immutable/nodes/4.d4385b49.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.8a8a1b5e.js";import{S as t,i as e}from"../chunks/index.a9c7f32e.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
