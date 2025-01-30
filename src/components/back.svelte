eac01bd8e351c873384538f9980e28ec015ee9d8
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2025-01-30 23:02:13 +0100

    add arrow to back button

--- src/components/back.svelte
+++ src/components/back.svelte
@@ -1,12 +1,35 @@
 <div class="fixed bottom-0 left-0 m-2">
     <a class="flex" href="/" aria-label="Return to home">
-        <div
-            class="rounded-full border-rose-100 border-4 dark:border-rose-400 select-none dark:border-opacity-30 backdrop-grayscale"
-        >
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="relative group">
             <div
-                class="bg-rose-600 rounded-full w-12 h-12 hover:scale-110 transition ease-in-out duration-500 cursor-pointer"
-            />
+                class="rounded-full border-rose-100 border-4 dark:border-rose-400 select-none dark:border-opacity-30 backdrop-grayscale"
+            >
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div
+                    class="bg-rose-600 rounded-full w-12 h-12 group-hover:scale-110 transition ease-in-out duration-500 cursor-pointer"
+                />
+            </div>
+            <div
+                class="text-black dark:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="0.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-8 w-8"
+                >
+                    <path d="M7 10h-4v4" />
+                    <path
+                        d="M21 12c-.887 -1.285 -2.48 -2.033 -4 -2c-1.52 -.033 -3.113 .715 -4 2c-.887 1.284 -2.48 2.033 -4 2c-1.52 .033 -3 -1 -4 -2l-2 -2"
+                    />
+                </svg>
+            </div>
         </div>
     </a>
 </div>
